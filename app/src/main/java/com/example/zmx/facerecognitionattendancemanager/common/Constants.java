264d5c43ff3bd95d560878ecd15b787afe8f4d64
iLoveCyaron <1023460580@qq.com>
2020-11-12 19:20:24 +0800

    添加ArcFace依赖和SDK API

---
+++ app/src/main/java/com/example/zmx/facerecognitionattendancemanager/common/Constants.java
@@ -0,0 +1,16 @@
+package com.example.zmx.facerecognitionattendancemanager.common;
+
+public class Constants {
+
+    public static final String APP_ID = "Ge96ZL6RUTwq4c4aGL5t2a7DHzq892dvJi2pjhfNL8qq";
+    public static final String SDK_KEY = "FnCeMyWHd3LW5VTuBG6Ds7kaB1HPDR5wtF8GMdtEuobS";
+
+    /**
+     * IR预览数据相对于RGB预览数据的横向偏移量，注意：是预览数据，一般的摄像头的预览数据都是 width > height
+     */
+    public static final int HORIZONTAL_OFFSET = 0;
+    /**
+     * IR预览数据相对于RGB预览数据的纵向偏移量，注意：是预览数据，一般的摄像头的预览数据都是 width > height
+     */
+    public static final int VERTICAL_OFFSET = 0;
+}
